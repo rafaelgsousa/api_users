dc65e3ce130195d25de79262c89ca2172cea4da0
rafaelgsousa <elderrafaelgomes@gmail.com>
2024-01-31 00:41:30 -0300

    implementing the refresh token url and refactoring the settings

--- project/settings.py
+++ project/settings.py
@@ -181,7 +181,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
--- project/urls.py
+++ project/urls.py
@@ -35,8 +35,7 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-# from rest_framework_simplejwt.views import (TokenObtainPairView,
-#                                             TokenRefreshView)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
@@ -45,7 +44,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
